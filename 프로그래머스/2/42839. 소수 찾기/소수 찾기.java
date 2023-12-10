bc0a11849c76330baa9246bd04820dde85293c47
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-10 20:06:46 +0900

    [level 2] Title: 소수 찾기, Time: 12.39 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,40 +1,46 @@
 import java.util.*;
 
 class Solution {
+//     set 자료구조 생성
     HashSet<Integer> numbersSet = new HashSet<>();
+
+//     순열 재귀 함수
+    public void recursive(String comb, String others) {
+        if(!comb.equals("")) 
+            numbersSet.add(Integer.valueOf(comb));
+        
+        // 아이디어 : 하나의 조합을 만든 후 다음 전달되는 문자열에서 한 개를 제거
+        // 예) 처음에 "", 127를 호출했으면 "1" 이라는 조합을 만들고 "27"를 매개변수로 전달함.
+        // recursive("", 127) -> recursive("1", 27) ... 순으로 실행됨.
+        // 만약 for문의 i가 증가하면 시작점이 변경되므로 recursive("2", 17) -> recursive("21", 7) ... 순으로 실행됨.
+        for(int i=0; i<others.length(); i++) 
+            recursive(comb+others.charAt(i), others.substring(0, i)+others.substring(i+1));
+    }
     
     public boolean isPrime(int num) {
         if(num == 0 || num == 1) return false;
         
         int lim = (int)Math.sqrt(num);
         
-        for(int i=2; i<=lim; i++) if(num%i == 0) return false;
+        for(int i=2; i<=lim; i++)
+            if(num%i == 0) return false;
         
         return true;
     }
     
-    public void recursive(String comb, String others) {
-        if(!comb.equals(""))
-            numbersSet.add(Integer.valueOf(comb));
-        
-        for(int i=0; i<others.length(); i++)
-            recursive(comb + others.charAt(i), others.substring(0,i) + others.substring(i+1));
-    }
-    
     public int solution(String numbers) {
-        // 모든 조합의 숫자 생성
         recursive("", numbers);
         
-        int count = 0;
-        // set 자료구조 순회 및 에라토스테네스의 체로 검사
+        int answer = 0;
+        
         Iterator<Integer> it = numbersSet.iterator();
         while(it.hasNext()) {
             int number = it.next();
-            if(isPrime(number))
-                count++;
+            if(isPrime(number)) {
+                answer++;
+            }
         }
         
-        // 카운팅된 값 반환
-        return count;
+        return answer;
     }
 }
