93aba2197afdb9edb2d97bf5eece3e0e5fb7ca0b
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2023-12-11 16:46:14 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,32 @@
+// DFS
+// 최소 필요 피로도보다 높은 체력인 경우에만 던전 출입 가능
+// 가능한 최대 던전 출입 횟수
+
+class Solution {
+    int fatigue;
+    int[][] map;
+    boolean[] visited;
+    int answer = 0;
+    
+    public void dfs(int depth, int fatigue, boolean[] visited) {               
+     
+        for(int i=0; i<map.length; i++) {
+            if(!visited[i] && map[i][0] <= fatigue) {
+                visited[i] = true;
+                dfs(depth+1, fatigue-map[i][1], visited);
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+    
+    public int solution(int k, int[][] dungeons) {
+        this.fatigue = k;
+        this.map = dungeons;
+        visited = new boolean[map.length];
+        
+        dfs(0, k, visited);
+        
+        return answer;
+    }
+}
